68b38a8c8777c6788bdb73ade1b1fa5b45f85a6f
Feth <fethfetov@gmail.com>
2025-03-01 23:42:19 +0700

    created mkdirs

---
+++ folders_default.sh
@@ -0,0 +1,15 @@
+# main device
+mkdir /mnt/1tb/system
+mkdir /mnt/1tb/system/pictures
+mkdir /mnt/1tb/system/pictures/high-priority
+mkdir /mnt/1tb/system/pictures/high-priority/wallpapers
+mkdir /mnt/1tb/system/pictures/high-priority/projects
+mkdir /mnt/1tb/system/pictures/low-priority/screenshots
+
+# secondary device
+mkdir /mnt/256g/system
+mkdir /mnt/256g/system/pictures
+mkdir /mnt/256g/system/pictures/high-priority
+mkdir /mnt/256g/system/pictures/high-priority/wallpapers
+mkdir /mnt/256g/system/pictures/high-priority/projects
+mkdir /mnt/256g/system/pictures/low-priority/screenshots
