fc63a45179c61b18c98c703fe2187f6dfb24af02
Feth <fethfetov@gmail.com>
2025-03-02 00:02:46 +0700

    Update folders_default.sh

--- folders_default.sh
+++ folders_default.sh
@@ -3,13 +3,27 @@ mkdir /mnt/1tb/system
 mkdir /mnt/1tb/system/pictures
 mkdir /mnt/1tb/system/pictures/high-priority
 mkdir /mnt/1tb/system/pictures/high-priority/wallpapers
-mkdir /mnt/1tb/system/pictures/high-priority/projects
+mkdir /mnt/1tb/system/pictures/high-priority/feth-made
 mkdir /mnt/1tb/system/pictures/low-priority/screenshots
+
+mkdir /mnt/1tb/system/documents
+mkdir /mnt/1tb/system/documents/high-priority
+mkdir /mnt/1tb/system/documents/high-priority/projects
+mkdir /mnt/1tb/system/documents/high-priority/projects/graphics
+mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/2d
+mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/3d
 
 # secondary device
 mkdir /mnt/256g/system
 mkdir /mnt/256g/system/pictures
 mkdir /mnt/256g/system/pictures/high-priority
 mkdir /mnt/256g/system/pictures/high-priority/wallpapers
-mkdir /mnt/256g/system/pictures/high-priority/projects
+mkdir /mnt/256g/system/pictures/high-priority/feth-made
 mkdir /mnt/256g/system/pictures/low-priority/screenshots
+
+mkdir /mnt/256g/system/documents
+mkdir /mnt/256g/system/documents/high-priority
+mkdir /mnt/256g/system/documents/high-priority/projects
+mkdir /mnt/256g/system/documents/high-priority/projects/graphics
+mkdir /mnt/256g/system/documents/high-priority/projects/graphics/2d
+mkdir /mnt/256g/system/documents/high-priority/projects/graphics/3d
