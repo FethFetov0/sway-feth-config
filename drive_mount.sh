22b484fcf914280a23894182b6251706b3da7ae6
Feth <fethfetov@gmail.com>
2025-03-02 00:15:41 +0700

    Update drive_mount.sh

--- drive_mount.sh
+++ drive_mount.sh
@@ -1,9 +1,9 @@
 # main device
 sudo mkdir     /mnt/1tb
 sudo mount     /dev/sda1 /mnt/1tb
-sudo chmod     ugo+wx /mnt/1tb
+sudo chmod     ugo+wx    /mnt/1tb
 
 # secondary device
 sudo mkdir     /mnt/256g
 sudo mount     /dev/sdb1
-sudo chmod     ugo+wx /mnt/256g
+sudo chmod     ugo+wx    /mnt/256g
