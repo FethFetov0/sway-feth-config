3cd2a19cae655d9bd3f956a49876b3e3671f3bc8
Feth <fethfetov@gmail.com>
2025-03-03 18:30:23 +0700

    Update folders_default.sh

--- shs/folders_default.sh
+++ shs/folders_default.sh
@@ -25,23 +25,3 @@ mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/3d/source-files/
 
 mkdir /mnt/1tb/system/pictures/low-priority
 mkdir /mnt/1tb/system/pictures/low-priority/screenshots
-
-# secondary device
-mkdir /mnt/256g/system
-
-mkdir /mnt/256g/system/pictures
-mkdir /mnt/256g/system/documents
-
-mkdir /mnt/256g/system/pictures/high-priority
-mkdir /mnt/256g/system/documents/high-priority
-
-mkdir /mnt/256g/system/pictures/high-priority/wallpapers
-mkdir /mnt/256g/system/pictures/high-priority/feth-made
-
-mkdir /mnt/256g/system/documents/high-priority/projects
-mkdir /mnt/256g/system/documents/high-priority/projects/graphics
-mkdir /mnt/256g/system/documents/high-priority/projects/graphics/2d
-mkdir /mnt/256g/system/documents/high-priority/projects/graphics/3d
-
-mkdir /mnt/256g/system/pictures/low-priority
-mkdir /mnt/256g/system/pictures/low-priority/screenshots
