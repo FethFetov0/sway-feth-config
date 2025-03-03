348aeb65db159a8ebdcf076901e471ac647f338f
Feth <fethfetov@gmail.com>
2025-03-03 18:30:04 +0700

    Update folders_default.sh

--- shs/folders_default.sh
+++ shs/folders_default.sh
@@ -11,9 +11,17 @@ mkdir /mnt/1tb/system/pictures/high-priority/wallpapers
 mkdir /mnt/1tb/system/pictures/high-priority/feth-made
 
 mkdir /mnt/1tb/system/documents/high-priority/projects
+mkdir /mnt/1tb/system/documents/high-priority/projects/game-dev
+mkdir /mnt/1tb/system/documents/high-priority/projects/game-dev/level-design
+mkdir /mnt/1tb/system/documents/high-priority/projects/game-dev/level-design/source
+mkdir /mnt/1tb/system/documents/high-priority/projects/game-dev/level-design/source-files
+mkdir /mnt/1tb/system/documents/high-priority/projects/game-dev/level-design/source-files/source-engine
+mkdir /mnt/1tb/system/documents/high-priority/projects/game-dev/level-design/source-files/source-engine/vmfs
 mkdir /mnt/1tb/system/documents/high-priority/projects/graphics
 mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/2d
 mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/3d
+mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/3d/source-files
+mkdir /mnt/1tb/system/documents/high-priority/projects/graphics/3d/source-files/blender
 
 mkdir /mnt/1tb/system/pictures/low-priority
 mkdir /mnt/1tb/system/pictures/low-priority/screenshots
