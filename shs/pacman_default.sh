452230208c5bd7e1e6419127ef3034e0a638f5b9
Feth <fethfetov@gmail.com>
2025-03-04 18:51:46 +0700

    Update pacman_default.sh

--- shs/pacman_default.sh
+++ shs/pacman_default.sh
@@ -1,1 +1,2 @@
 sudo pacman -S lemurs alacritty ranger nemo ark git rofi picom discord firefox maim ntfs-3g
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
