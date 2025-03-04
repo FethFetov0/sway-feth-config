37a26401d2899919fa12b3dd36ce88888ac72b3a
Feth <fethfetov@gmail.com>
2025-03-04 19:01:41 +0700

    Update and rename pacman_default.sh to download_default.sh

--- shs/download_default.sh
+++ shs/download_default.sh
@@ -1,2 +1,3 @@
 sudo pacman -S lemurs alacritty ranger nemo ark git rofi picom discord firefox maim ntfs-3g
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+firefox --new-tab https://github.com/bol-van/zapret/releases/download/v70.3/zapret-v70.3.tar.gz
